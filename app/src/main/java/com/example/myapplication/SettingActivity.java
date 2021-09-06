00812a4ef6760f2c20fd1a23d314a83ebbd98aa0
jsBear92_iMac <js104427@gmail.com>
2021-09-06 15:20:10 +0800

    FEAT: add navigation view instead of menu buttons
    - navigation view has 3 menus

--- app/src/main/java/com/example/myapplication/SettingActivity.java
+++ app/src/main/java/com/example/myapplication/SettingActivity.java
@@ -1,52 +1,59 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class SettingActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
-        // Buttons (Main Menus)
-        Button buttonMain = findViewById(R.id.button2);
-        Button buttonAnalysis = findViewById(R.id.button3);
-        Button buttonSettings = findViewById(R.id.button4);
+        // get drawer for navigation view
+        drawerLayout = findViewById(R.id.drawer);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // Call Main View (Refresh)
-        buttonMain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        // Navigation Highlight
+        NavigationView navigationView = this.findViewById(R.id.navigation_view);
+        navigationView.setCheckedItem(R.id.settings_activity);
+    }
+
+    // Change Activity
+    public void changeActivity(MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.main_activity:
+                Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
                 finish();
-            }
-        });
-
-        // Call Analysis View
-        buttonAnalysis.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), AnalysingActivity.class);
+                break;
+            case R.id.analysis_activity:
+                intent = new Intent(this, AnalysingActivity.class);
                 startActivity(intent);
                 finish();
-            }
-        });
+                break;
+        }
+    }
 
-        // Call Settings View
-        buttonSettings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), SettingActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggle.onOptionsItemSelected(item)) return true;
+        return super.onOptionsItemSelected(item);
     }
 }
